7b3b2b46cbd968df9f4de76d60df04435b91778c
Andre <webmaster@andre-sieverding.de>
2019-07-24 16:25:21 +0200

    Update synchronize.php

--- synchronize.php
+++ synchronize.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Author: Andre Sieverding
@@ -5,11 +6,17 @@
  */
 
 // This file is using the mapping.json configuration file!
+// Check SAPI for command line interface
+if (PHP_SAPI !== 'cli') {
+    echo 'This script must be run as a CLI application';
+    die();
+}
 
 // Set timelimit for script execution
 set_time_limit(0);
 
 // Check mapping.json
+echo "Check mapping.json ...\r\n";
 if (!file_exists('mapping.json')) {
 	echo 'File mapping.json don\'t exists!';
 	die();
@@ -49,11 +56,13 @@ $errorLogFileHandle = fopen('log/' . date('Y-m-d') . '_error-log.txt', 'w');
 $queryLogFileHandle = fopen('log/' . date('Y-m-d') . '_query-log.txt', 'w');
 
 // Connect to Oracle Database
+echo "Connecting to Oracle database ...\r\n";
 $oraDB = oci_pconnect($mapping->source->user, $mapping->source->password, $mapping->source->descriptionString, 'AL32UTF8');
 
 // Export data from Oracle Database and create a sql import file for Microsoft SQL Server (for each table)
 foreach ($mapping->tables as $table) {
 	// Check all required fields from mapping.json file for current table
+	echo "Check mapping.json for current table ...\r\n";
 	if (!(isset($table->source)
 		&& !empty($table->source)
 		&& isset($table->source->table)
@@ -74,7 +83,10 @@ foreach ($mapping->tables as $table) {
 		die();
 	}
 
+	echo "Tables: " . $table->source->table . " (Oracle) -> " . $mapping->dest->scheme . "." . $table->dest->table->merge . " (SQL Server) ...\r\n";
+
 	// Build sql query
+	echo "Build SELECT query for Oracle ...\r\n";
 	$query = "SELECT " . implode(', ', $table->source->fields) . " FROM " . $table->source->table;
 
 	// Build WHERE statement for sql query if where is in mapping.json included
@@ -103,6 +115,7 @@ foreach ($mapping->tables as $table) {
 	fwrite($queryLogFileHandle, date('Y-m-d H:i:s') . " [query]: " . $query . "\r\n");
 
 	// Execute query
+	echo "Execute query ...\r\n";
 	$statement = oci_parse($oraDB, $query);
 	oci_execute($statement);
 
@@ -110,6 +123,8 @@ foreach ($mapping->tables as $table) {
 	if ($statement) {
 		// Create file handler
 		$fileHandle = fopen('cache/' . $table->dest->table->merge . '.sql', 'w');
+
+		echo "Build " . $table->dest->table->merge . ".sql file ...\r\n";
 
 		while (($row = oci_fetch_array($statement, OCI_NUM)) != false) {
 			// Build INSERT INTO sql queries
@@ -125,14 +140,16 @@ foreach ($mapping->tables as $table) {
 		// Close file handler
 		fclose($fileHandle);
 	} else {
-		fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Table " . $table->source->table . " konnte nicht synchronisiert werden!\r\n");
+		fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Table " . $table->source->table . " couldn't be synced!\r\n");
 	}
 }
 
 // Close Oracle Database connection
+echo "Close Oracle connection ...\r\n";
 oci_close($oraDB);
 
 // Connect to SQL Server
+echo "Connect to Microsoft SQL Server " . $mapping->dest->user . "@" . $mapping->dest->host . " ...\r\n";
 $sqlsrvDB = \sqlsrv_connect($mapping->dest->host, [
 	"Database" => $mapping->dest->database,
 	"Uid" => $mapping->dest->user,
@@ -140,9 +157,14 @@ $sqlsrvDB = \sqlsrv_connect($mapping->dest->host, [
 ]);
 
 // Import all sql files to SQL Server database
+echo "Import all sql files to SQL Server ...\r\n";
 foreach ($mapping->tables as $table) {
 	// Check whether sql file for current table exists
 	if (file_exists('cache/' . $table->dest->table->merge . '.sql')) {
+		// Truncate import table
+		$query = "TRUNCATE TABLE " . $mapping->dest->scheme . "." . $table->dest->table->import . ";";
+		sqlsrv_query($sqlsrvDB, $query);
+
 		// Create file handler
 		$fileHandle = fopen('cache/' . $table->dest->table->merge . '.sql', 'r');
 
@@ -155,7 +177,7 @@ foreach ($mapping->tables as $table) {
 				$result = sqlsrv_query($sqlsrvDB, $query);
 
 				if (!$result) {
-					fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Query couldn\'t be performed: " . $query . "\r\n");
+					fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Query couldn't be performed: " . $query . "\r\n");
 				}
 			}
 		}
@@ -164,18 +186,25 @@ foreach ($mapping->tables as $table) {
 		fclose($fileHandle);
 
 		// Delete sql file from cache
+		echo "Delete " . $table->dest->table->merge . ".sql ...\r\n";
 		unlink('cache/' . $table->dest->table->merge . '.sql');
 	} else {
-		fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Table " . $table->source->table . " konnte nicht synchronisiert werden!\r\n");
+		fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [error]: Table " . $table->source->table . " couldn't be synced!\r\n");
 	}
 }
 
+/**
+ * MERGE
+ */
+// code ...
+
 // Close SQl Server database connection
+echo "Close SQL Server connection ...\r\n";
 sqlsrv_close($sqlsrvDB);
 
 // Done!
 fwrite($errorLogFileHandle, date('Y-m-d H:i:s') . " [success]: Done!\r\n");
-echo 'Done!';
+echo "Done!\r\n";
 
 // At least, close log file handler
 fclose($errorLogFileHandle);
